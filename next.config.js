a94a319c683d100af612cd3a7313ee0cc7454285
miri-clerobotics <miri@clerobotics.com>
2026-06-12 14:20:36 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
-  latex: true,
-  flexsearch: {
-    codeblocks: false
-  },
-  defaultShowCopyCode: true
 })
 
 module.exports = withNextra({
